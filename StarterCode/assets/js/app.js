2407f47dc1ea4d2f3a357b0682a1268011eeadd0
Peter <46304666+peterjorphan@users.noreply.github.com>
2019-05-09 18:06:22 -0500

    loaded data

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,10 @@
-// @TODO: YOUR CODE HERE!
+// Load data from data.csv
+d3.csv("assets/data/data.csv", function(error, data) {
+
+    // Log an error if one exists
+    if (error) return console.warn(error);
+  
+    // Print the Data
+    console.log(data);
+
+});
