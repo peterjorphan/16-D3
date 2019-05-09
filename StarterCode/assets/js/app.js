de8c1d2d7b52f36b8ed51dd246add2e5b9612a41
Peter <46304666+peterjorphan@users.noreply.github.com>
2019-05-09 18:24:26 -0500

    add svg

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,3 +1,29 @@
+// Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 660;
+
+// Define the chart's margins as an object
+var chartMargin = {
+  top: 30,
+  right: 30,
+  bottom: 30,
+  left: 30
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+// Append a group to the SVG area and shift ('translate') it to the right and to the bottom
+var chartGroup = svg.append("g")
+.attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
 // Load data from data.csv
 d3.csv("assets/data/data.csv", function(error, data) {
 
